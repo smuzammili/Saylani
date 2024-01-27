4a813547daf90405b7da62d7c11ac837548dad90
smuzammili <smuzammili@gmail.com>
2024-01-27 22:56:52 +0500

    Q5 complete

--- task-3/Q4.dart
+++ task-3/Q4.dart
@@ -2,6 +2,7 @@
 //number from a list.
 void main() {
   var numbers = [20, 15, 10, 5, 37, 28, 40, 50];
-  var smallest = [];
-  var greater = [];
+  numbers.sort();
+  print("smallest number in the list: ${numbers.first}");
+  print("greatest number in the list: ${numbers.last}");
 }
--- task-3/Q5.dart
+++ task-3/Q5.dart
@@ -1,1 +1,13 @@
-
+// Q.5: Create a map with name, phone keys and store some values to it. Use
+// where to find all keys that have length 4.
+void main() {
+  Map names = {
+    03331234567: 50,
+    03211234567: 100,
+    03191234567: 200,
+    03001234567: 300
+  };
+  for (int i in names.keys) {
+    print(i);
+  }
+}
